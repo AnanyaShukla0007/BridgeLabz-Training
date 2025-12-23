f8f0e64ab797ee23b11a9450976fa25f3f8c8f8b
Ananya Shukla <ananyashukla0307@gmail.com>
2025-12-23 16:19:06 +0530

    Added: Control Flow Level 3

--- core-csharp-practice/gcr-codebase/c#-control-flow/Level3/Calculator.cs
+++ core-csharp-practice/gcr-codebase/c#-control-flow/Level3/Calculator.cs
@@ -1,28 +1,31 @@
 using System;
-class Calculator{
-    static void Main(){
-        Console.WriteLine("Enter first number: ");
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
         double firstnum = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter second number: ");
+        Console.WriteLine("Enter second number:");
         double secondnum = double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter operator (+,-,*,/): )")
-        string opo=Console.Readline();
-        switch(opo){
+        Console.WriteLine("Enter operator (+, -, *, /):");
+        string op = Console.ReadLine();
+        switch (op)
+        {
             case "+":
-                Console.WriteLine("Result = " + (first + second));
+                Console.WriteLine("Result = " + (firstnum + secondnum));
                 break;
             case "-":
-                Console.WriteLine("Result = " + (first - second));
+                Console.WriteLine("Result = " + (firstnum - secondnum));
                 break;
             case "*":
-                Console.WriteLine("Result = " + (first * second));
+                Console.WriteLine("Result = " + (firstnum * secondnum));
                 break;
             case "/":
-                Console.WriteLine("Result = " + (first / second));
+                Console.WriteLine("Result = " + (firstnum / secondnum));
                 break;
             default:
                 Console.WriteLine("Invalid Operator");
                 break;
-        } 
+        }
     }
 }
