5d4acceb2540389397644fb13160f9f5d1e90c96
Ananya Shukla <ananyashukla0307@gmail.com>
2025-12-31 22:04:32 +0530

    Added: Banking System using MultiClass 31/12/25

---
+++ oops-c#-practice/scenario-codebase/Bank System/Program.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main()
+    {
+        BankSystemManager manager = new BankSystemManager();
+
+        manager.CreateUser();
+        manager.CreateAccount();
+
+        manager.Deposit(5000);
+        manager.Withdraw(2000);
+        manager.CheckBalance();
+
+        manager.ShowCompleteDetails();
+    }
+}
