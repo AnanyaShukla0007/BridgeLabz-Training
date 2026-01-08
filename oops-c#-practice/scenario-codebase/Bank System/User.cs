cd7a1c345ba5ee92aeb7db1a11dc080bbd5a8896
Ananya Shukla <ananyashukla0307@gmail.com>
2026-01-08 22:00:38 +0530

    Removed unnecessary files

--- oops-c#-practice/scenario-codebase/Bank System/User.cs
+++
@@ -1,9 +0,0 @@
-class User
-{
-    public string name;
-    public string dateOfBirth;
-    public string aadhaarNumber;
-    public int customerId;
-    public string accountType;
-    public string registeredMobile;
-}
--- oops-c#-practice/scenario-codebase/VehicleRentalApp/IRentable.cs
+++
@@ -1,5 +0,0 @@
-using System;
-// Interface IRentable 
-public interface IRentable{
-  double CalculateRent(int day);
-}
