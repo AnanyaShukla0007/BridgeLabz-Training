fe36d712a41df2e0f05eeb20ff4086471db249e1
Ananya Shukla <ananyashukla0307@gmail.com>
2026-01-09 15:13:25 +0530

    Ananya added ATM Dispenser Practice Problem 9/1/26

---
+++ dsa-c#-practice/scenario-codebase/ATMDispenser/ATM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraning.ATMDispenser
+{
+    internal class ATM
+    {
+        public int[] Notes;
+
+        public ATM(int[] notes)
+        {
+            Notes = notes;
+        }
+    }
+}
