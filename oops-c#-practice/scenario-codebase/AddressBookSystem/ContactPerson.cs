a1924c53f5cdb89835e9acf51919e2e2b2bf39a6
Ananya Shukla <ananyashukla0307@gmail.com>
2026-01-14 12:16:35 +0530

    UC2: Added ability to edit contact with clear UC-wise comments. 14/1/26

--- oops-c#-practice/scenario-codebase/AddressBookSystem/ContactPerson.cs
+++ oops-c#-practice/scenario-codebase/AddressBookSystem/ContactPerson.cs
@@ -5,8 +5,11 @@ using System.Text;
 
 namespace AddressBookSystem
 {
+    // ContactPerson class represents a single contact person
+    // UC-1: Creation of Contact Person with required details
     class ContactPerson
     {
+        // UC-1: Contact attributes
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
