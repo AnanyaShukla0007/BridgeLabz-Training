bdf050139ed4e87b3aa0c989a76eea9f599ef829
Ananya Shukla <ananyashukla0307@gmail.com>
2026-01-14 12:11:06 +0530

    UC1: Added ability to add a new contact using OOP and Console input. 14/1/26

---
+++ oops-c#-practice/scenario-codebase/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        private ContactPerson contact;
+
+        public void AddContact(ContactPerson person)
+        {
+            contact = person;
+            Console.WriteLine("\nContact added successfully!");
+        }
+
+        public void DisplayContact()
+        {
+            Console.WriteLine("\n--- Contact Details ---");
+            Console.WriteLine("Name: " + contact.FirstName + " " + contact.LastName);
+            Console.WriteLine("Address: " + contact.Address);
+            Console.WriteLine("City: " + contact.City);
+            Console.WriteLine("State: " + contact.State);
+            Console.WriteLine("Zip: " + contact.Zip);
+            Console.WriteLine("Phone: " + contact.PhoneNumber);
+            Console.WriteLine("Email: " + contact.Email);
+        }
+    }
+}
