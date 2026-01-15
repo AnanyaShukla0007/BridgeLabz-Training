6b4da94b5c444aff5332781fc0cf623f3bad9020
Ananya Shukla <ananyashukla0307@gmail.com>
2026-01-15 23:57:31 +0530

    Added support for multiple Address Books using custom linked list 15/1/26

--- dsa-c#-practice/scenario-codebase/AddressBookSystem/AddressBook.cs
+++ dsa-c#-practice/scenario-codebase/AddressBookSystem/AddressBook.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    // UC-4: Node class for Linked List
+    // UC-4: Node for Contact Linked List
     class ContactNode
     {
         public ContactPerson Data;
@@ -19,13 +19,14 @@ namespace AddressBookSystem
         }
     }
 
-    // AddressBook handles all contact operations
+    // UC-5: AddressBook with unique name
     class AddressBook
     {
-        // UC-4: Head node of Linked List
+        public string AddressBookName { get; set; }   // UC-5
+
         private ContactNode head = null;
 
-        // UC-1 & UC-4: Add contact to Address Book
+        // UC-4: Add contact
         public void AddContact(ContactPerson person)
         {
             ContactNode newNode = new ContactNode(person);
@@ -38,88 +39,24 @@ namespace AddressBookSystem
             {
                 ContactNode temp = head;
                 while (temp.Next != null)
-                {
                     temp = temp.Next;
-                }
+
                 temp.Next = newNode;
             }
-
-            Console.WriteLine("Contact added successfully!");
         }
 
-        // UC-2: Edit contact using First Name
-        public void EditContact(string firstName)
+        // UC-4: Display contacts
+        public void DisplayContacts()
         {
-            ContactNode temp = head;
+            Console.WriteLine("\nAddress Book: " + AddressBookName);
 
-            while (temp != null)
-            {
-                if (temp.Data.FirstName.Equals(firstName))
-                {
-                    Console.Write("Enter New City: ");
-                    temp.Data.City = Console.ReadLine();
-
-                    Console.Write("Enter New State: ");
-                    temp.Data.State = Console.ReadLine();
-
-                    Console.Write("Enter New Phone Number: ");
-                    temp.Data.PhoneNumber = Console.ReadLine();
-
-                    Console.WriteLine("Contact updated successfully!");
-                    return;
-                }
-                temp = temp.Next;
-            }
-
-            Console.WriteLine("Contact not found!");
-        }
-
-        // UC-3: Delete contact using First Name
-        public void DeleteContact(string firstName)
-        {
             if (head == null)
             {
                 Console.WriteLine("No contacts available.");
                 return;
             }
 
-            if (head.Data.FirstName.Equals(firstName))
-            {
-                head = head.Next;
-                Console.WriteLine("Contact deleted successfully!");
-                return;
-            }
-
-            ContactNode prev = head;
-            ContactNode curr = head.Next;
-
-            while (curr != null)
-            {
-                if (curr.Data.FirstName.Equals(firstName))
-                {
-                    prev.Next = curr.Next;
-                    Console.WriteLine("Contact deleted successfully!");
-                    return;
-                }
-                prev = curr;
-                curr = curr.Next;
-            }
-
-            Console.WriteLine("Contact not found!");
-        }
-
-        // UC-4: Display all contacts
-        public void DisplayAllContacts()
-        {
-            if (head == null)
-            {
-                Console.WriteLine("No contacts to display.");
-                return;
-            }
-
-            Console.WriteLine("\n--- Address Book Contacts ---");
             ContactNode temp = head;
-
             while (temp != null)
             {
                 Console.WriteLine(
--- dsa-c#-practice/scenario-codebase/AddressBookSystem/AddressBookMain.cs
+++ dsa-c#-practice/scenario-codebase/AddressBookSystem/AddressBookMain.cs
@@ -6,45 +6,111 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
+    // UC-5: Node for AddressBook Linked List
+    class AddressBookNode
+    {
+        public AddressBook Data;
+        public AddressBookNode Next;
+
+        public AddressBookNode(AddressBook book)
+        {
+            Data = book;
+            Next = null;
+        }
+    }
+
+    // UC-5: AddressBookSystem manages multiple AddressBooks
+    class AddressBookSystem
+    {
+        private AddressBookNode head = null;
+
+        // UC-5: Add new Address Book
+        public void AddAddressBook(AddressBook book)
+        {
+            AddressBookNode newNode = new AddressBookNode(book);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                AddressBookNode temp = head;
+                while (temp.Next != null)
+                    temp = temp.Next;
+
+                temp.Next = newNode;
+            }
+        }
+
+        // UC-5: Display all Address Books
+        public void DisplayAllAddressBooks()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No Address Books found.");
+                return;
+            }
+
+            AddressBookNode temp = head;
+            while (temp != null)
+            {
+                temp.Data.DisplayContacts();
+                temp = temp.Next;
+            }
+        }
+    }
+
     class AddressBookMain
     {
         static void Main(string[] args)
         {
-            // UC-1: Welcome message
             Console.WriteLine("Welcome to Address Book Program");
 
-            AddressBook addressBook = new AddressBook();
+            AddressBookSystem system = new AddressBookSystem();
             string choice;
 
-            // UC-4: Add multiple contacts
+            // UC-5: Add multiple Address Books
             do
             {
-                ContactPerson person = new ContactPerson();
+                AddressBook book = new AddressBook();
 
-                Console.Write("Enter First Name: ");
-                person.FirstName = Console.ReadLine();
+                Console.Write("\nEnter Address Book Name: ");
+                book.AddressBookName = Console.ReadLine();
 
-                Console.Write("Enter Last Name: ");
-                person.LastName = Console.ReadLine();
+                string addContactChoice;
+                do
+                {
+                    ContactPerson person = new ContactPerson();
 
-                Console.Write("Enter City: ");
-                person.City = Console.ReadLine();
+                    Console.Write("Enter First Name: ");
+                    person.FirstName = Console.ReadLine();
 
-                Console.Write("Enter State: ");
-                person.State = Console.ReadLine();
+                    Console.Write("Enter Last Name: ");
+                    person.LastName = Console.ReadLine();
 
-                Console.Write("Enter Phone Number: ");
-                person.PhoneNumber = Console.ReadLine();
+                    Console.Write("Enter City: ");
+                    person.City = Console.ReadLine();
 
-                addressBook.AddContact(person);
+                    Console.Write("Enter Phone Number: ");
+                    person.PhoneNumber = Console.ReadLine();
 
-                Console.Write("Add another contact? (yes/no): ");
+                    book.AddContact(person);
+
+                    Console.Write("Add another contact to this Address Book? (yes/no): ");
+                    addContactChoice = Console.ReadLine();
+
+                } while (addContactChoice.Equals("yes"));
+
+                system.AddAddressBook(book);
+
+                Console.Write("Add another Address Book? (yes/no): ");
                 choice = Console.ReadLine();
 
             } while (choice.Equals("yes"));
 
-            // UC-4: Display all contacts
-            addressBook.DisplayAllContacts();
+            // UC-5: Display all Address Books
+            system.DisplayAllAddressBooks();
         }
     }
 }
