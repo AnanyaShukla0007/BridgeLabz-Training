1bfe723c46c88af306418bb5ff2bf02c10be13e4
Ananya Shukla <ananyashukla0307@gmail.com>
2026-01-27 11:00:16 +0530

    Ananya committed Address Book System on 27/1/26.

---
+++ collections-c#-practice/scenario-codebase/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    // Custom Exception – UC-6 / UC-7
+    class DuplicateContactException : Exception
+    {
+        public DuplicateContactException(string message) : base(message) { }
+    }
+
+    class AddressBook
+    {
+        public string AddressBookName { get; set; }
+
+        // UC-4: Multiple contacts using Collection
+        private List<ContactPerson> contacts = new List<ContactPerson>();
+
+        // UC-6 & UC-7: Add contact with duplicate prevention
+        public void AddContact(ContactPerson person)
+        {
+            if (contacts.Contains(person))
+                throw new DuplicateContactException("Duplicate contact entry not allowed.");
+
+            contacts.Add(person);
+        }
+
+        // UC-2: Edit contact
+        public void EditContact(string firstName)
+        {
+            ContactPerson person = contacts.Find(p => p.FirstName.Equals(firstName));
+
+            if (person == null)
+                throw new Exception("Contact not found.");
+
+            Console.Write("Enter New City: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter New State: ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter New Phone: ");
+            person.PhoneNumber = Console.ReadLine();
+        }
+
+        // UC-3: Delete contact
+        public void DeleteContact(string firstName)
+        {
+            ContactPerson person = contacts.Find(p => p.FirstName.Equals(firstName));
+
+            if (person == null)
+                throw new Exception("Contact not found.");
+
+            contacts.Remove(person);
+        }
+
+        // UC-10: Sort by name
+        public void SortByName()
+        {
+            contacts.Sort((a, b) => a.FirstName.CompareTo(b.FirstName));
+        }
+
+        // UC-11: Sort by City / State / Zip
+        public void SortByCity() => contacts.Sort((a, b) => a.City.CompareTo(b.City));
+        public void SortByState() => contacts.Sort((a, b) => a.State.CompareTo(b.State));
+        public void SortByZip() => contacts.Sort((a, b) => a.Zip.CompareTo(b.Zip));
+
+        // UC-4 / UC-10 / UC-11: Display
+        public void DisplayContacts()
+        {
+            Console.WriteLine($"\nAddress Book: {AddressBookName}");
+            foreach (var person in contacts)
+                Console.WriteLine(person);
+        }
+
+        // UC-8 & UC-9 helper
+        public List<ContactPerson> GetContacts()
+        {
+            return contacts;
+        }
+    }
+}
---
+++ collections-c#-practice/scenario-codebase/AddressBookSystem/ContactPerson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class ContactPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        // UC-7: Logical equality for duplicate detection
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is ContactPerson))
+                return false;
+
+            ContactPerson other = (ContactPerson)obj;
+            return FirstName.Equals(other.FirstName)
+                && LastName.Equals(other.LastName);
+        }
+
+        public override int GetHashCode()
+        {
+            return (FirstName + LastName).GetHashCode();
+        }
+
+        // UC-10 & UC-11: Clean display
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} | {City} | {State} | {Zip} | {PhoneNumber}";
+        }
+    }
+}
